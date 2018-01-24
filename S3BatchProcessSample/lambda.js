7cf8b3685655ce738805581a0a0ce164da89e5d6
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-24 07:44:50 +0530

    Initial commit

---
+++ S3BatchProcessSample/lambda.js
@@ -0,0 +1,38 @@
+let AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+exports.handler = function (event, context, callback) {
+
+	console.log(`Batch process triggered at ${event.time}`);
+
+	s3.listObjects({
+		'Bucket': 'slapp-batch-process-sample',
+		'MaxKeys': 100,
+		'Prefix': ''
+	}).promise()
+		.then(data => {
+			let numFiles = data.Contents.length;
+			console.log(`${numFiles} files found to process`);
+			data.Contents.forEach(file => {
+				let fileName = file.Key;
+				console.log(`Processing File : ${fileName}`);
+
+				s3.deleteObject({
+					'Bucket': "slapp-batch-process-sample",
+					'Key': fileName
+				}).promise()
+					.then(data => {
+						console.log(`Successfully deleted file ${fileName}`); 
+					})
+					.catch(err => {
+						console.log(`Failed to delete file : ${fileName}`, err, err.stack);
+						callback(err, `Failed to delete file : ${fileName}`);
+					});
+			});
+		})
+		.catch(err => {
+			console.log("Failed to get file list", err, err.stack); // an error occurred
+			callback(err, "Failed to get file list");
+		});
+
+	callback(null, 'Successfully executed');
+}
