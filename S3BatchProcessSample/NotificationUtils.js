21787c3a3894ae217af48b876465681a920e3166
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-24 10:56:06 +0530

    Fixed notification utils for node js

--- S3BatchProcessSample/NotificationUtils.js
+++ S3BatchProcessSample/NotificationUtils.js
@@ -1,18 +1,22 @@
 let AWS = require('aws-sdk');
 const sns = new AWS.SNS();
 
-export const sendNotification = (subject = '', message) => {
-    sns.publish({
-        Message: message,
-        Subject: subject,
-        MessageAttributes: {},
-        MessageStructure: 'String',
-        TopicArn: 'arn:aws:sns:us-east-1:480964559519:S3BatchProcessNotifications'
-    }).promise()
-        .then(data => {
-            console.log("Successfully published notification");
-        })
-        .catch(err => {
-            console.err("Error occurred while publishing notification", err, err.stack);
-        });
+module.exports = {
+    sendNotification: function (subject, message) {
+        sns.publish({
+            Message: message,
+            Subject: subject,
+            MessageAttributes: {},
+            MessageStructure: 'String',
+            TopicArn: 'arn:aws:sns:us-east-1:480964559519:S3BatchProcessNotifications'
+        }).promise()
+            .then(data => {
+                console.log("Successfully published notification");
+                return true;
+            })
+            .catch(err => {
+                console.err("Error occurred while publishing notification", err, err.stack);
+                throw err;
+            });
+    }
 };
